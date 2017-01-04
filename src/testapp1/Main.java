c916dad8f9908f0ad195beadab4fe17e46f080aa
Luigi <giggitnf@gmail.com>
2017-01-04 14:48:35 +0100

    Upload Files

---
+++ src/testapp1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testapp1;
+
+/**
+ *
+ * @author luigi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        testapp1.NewFrame.main(args);
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+    }
+
+}
